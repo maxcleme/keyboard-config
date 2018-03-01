193aeb08b3b9e604367ad48278e97c2e6aa42aa1
Maxime CLEMENT <mclement@xee.com>
2018-03-01 19:51:35 +0100

    Add XD64 led scripts

---
+++ xd64/scripts/led_green_off.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Beware that /usr/share/X11/xkb/compat/ledscroll shouldn't contains '!allowExplicit' but 'allowExplicit'
+
+xset -led named 'Scroll Lock'
---
+++ xd64/scripts/led_red_off.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Beware that /usr/share/X11/xkb/compat/lednum shouldn't contains '!allowExplicit' but 'allowExplicit'
+
+xset -led named 'Num Lock'
